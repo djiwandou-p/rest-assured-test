f20e485016ea644c4fc4871435801b0791367243
djiwandou-p <djiwandou@evermos.com>
2021-04-17 08:38:19 +0700

    init maven project

---
+++ src/test/java/com/evm/restassured/AppTest.java
@@ -0,0 +1,20 @@
+package com.evm.restassured;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
